47cb9a2aa22a0285761369b96bbe29dd421312a6
Randy Willey <rwilley966@gmail.com>
2018-06-05 15:21:10 -0400

    Module 01 Submissions

---
+++ Module_01/Lab_01/Lab_01/Lab_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Module_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 15;
+            int num2 = 30;
+            int answer1;
+            int answer2;
+
+            answer1 = num1 + num2;
+            answer2 = num2 / num1;
+
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " = " + answer1);
+            Console.WriteLine(num2 + " divided by " + num1 + " = " + answer2);
+        }
+    }
+}
