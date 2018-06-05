bf22a17577f54fde6f95c64d01e80322b7a9d0b3
Randy Willey <rwilley966@gmail.com>
2018-06-05 15:28:38 -0400

    Module 05 Lab 1-2 Submissions

---
+++ Module_05/Mod05Lab01/Mod05Lab01/Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mod05Lab01
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string Mobile;
+        public string Email;
+    }
+}
---
+++ Module_05/Mod05Lab01/Mod05Lab01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Mod05Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string personStr = File.ReadAllText("people.json");
+            List<Person> PersonList = new List<Person>();
+            try
+            {
+                PersonList = JsonConvert.DeserializeObject<List<Person>>(personStr);
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                Console.WriteLine("First Name     | Last Name      | Mobile #       | Email");
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                foreach (var p in PersonList)
+                {
+                    Console.WriteLine(String.Format("{0,-14} | {1,-14} | {2,-10} | {3,-10}", p.FirstName, p.LastName, p.Mobile, p.Email));
+                }
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
